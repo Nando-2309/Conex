056741fa6565bb114579185f908cc4e1434efa9c
Leandro <leandro.nscmt@gmail.com>
2025-08-06 10:40:41 -0300

    acertando o header e introduzindo Buffer

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const axios = require('axios');
-const qs = require('qs'); // <-- adicione isso
+const qs = require('qs');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// URL de redirecionamento (a mesma configurada na Conta Azul)
 const redirectUri = 'https://api-contaazul.onrender.com/oauth2/callback';
 
-// Use variáveis de ambiente no Render
 const clientId = process.env.CLIENT_ID;
 const clientSecret = process.env.CLIENT_SECRET;
 
@@ -34,7 +32,8 @@ app.get('/oauth2/callback', async (req, res) => {
       }),
       {
         headers: {
-          'Content-Type': 'application/x-www-form-urlencoded'
+          'Content-Type': 'application/x-www-form-urlencoded',
+          'Authorization': 'Basic ' + Buffer.from(`${clientId}:${clientSecret}`).toString('base64')
         }
       }
     );
